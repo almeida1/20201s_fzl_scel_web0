adc48f96a3b49ed2ecc2002404834acb165217db
almei <almei@192.168.0.6>
2020-01-19 10:27:55 -0300

    5 - Configurando permissoes para usuarios

--- scel/src/main/java/com/fatec/scel/security/WebSecurityConfig.java
+++ scel/src/main/java/com/fatec/scel/security/WebSecurityConfig.java
@@ -1,17 +1,40 @@
 package com.fatec.scel.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+		.antMatchers("/usuarios/cadastrar").hasAnyRole("ADMIN")
+		.antMatchers("/livros/cadastrar").hasAnyRole("USUARIO")
 		.anyRequest().authenticated()
 		.and()
 		.formLogin().loginPage("/login")
-		.permitAll();
+		.permitAll()
+		.and().logout().permitAll();
+	}
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			
+			.withUser("carlos").password(pc().encode("123")).roles("ADMIN")
+			.and()
+			.withUser("flavio").password(pc().encode("123")).roles("USUARIO");
+	}
+	@Bean
+	public BCryptPasswordEncoder pc() {
+		return new BCryptPasswordEncoder();
 	}
 }
